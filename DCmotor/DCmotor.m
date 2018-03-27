967558716cdc1d690fab564ff9a405b8a2ab3fe2
Thomas Van Riel <thomas.van.riel@gmail.com>
2018-03-27 10:09:42 +0200

    Corrected Kv value

--- DCmotor/DCmotor.m
+++ DCmotor/DCmotor.m
@@ -3,7 +3,7 @@ classdef DCmotor
         Tms=0.0232;         % Stall torque [Nm]
         Ra=3.32;            % Terminal Resistance [Ohm]
         Kt=0.0085;          % Torque constant [Nm/A]
-        Kv=1120/60*2*pi;    % speed constant [rad/Vs]
+        Kv=1120/(60*2*pi);  % speed constant [rad/Vs]
         Ke=0.0085;          % Inverse of the speed constant [Vs/rad]
         Eff=0.84;           % Max efficiency
     end
